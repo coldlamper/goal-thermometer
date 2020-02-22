06f8346b8672c752fdaacadea558ace4f0fa24c5
Brian Keith <brian@gigsalad.com>
2020-02-22 01:06:46 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,36 @@
+$.fn.thermometer = function (current) {
+  $('.current').text(current);
+	var $therm = this;
+	//this.append('<div class="meter"></div>');
+	
+	var	$goal = $therm.find('.goal'),
+			$current = $therm.find('.current'),
+			$meter = $therm.find('.meter'),
+			goalNum = $goal.text().replace(/\D/g, ''), //strip non-numbers from amount
+			currentNum = $current.text().replace(/\D/g, ''), //strip non-numbers from amount
+			thermHeight = 650,
+			reached = currentNum / goalNum,
+			level = thermHeight - (thermHeight * reached);
+	
+	$current.css({
+		'top': level - ($current.height() / 2) + 'px'
+	});
+
+	$meter.css({
+		'height': thermHeight - level + 'px'
+	});
+
+	if (goalNum == currentNum) {
+		$meter.css({
+			'-webkit-border-radius': '52px 52px 0 0',
+			'-moz-border-radius': '52px 52px 0 0',
+			'border-radius': '52px 52px 0 0'
+		});
+	}
+
+}
+let goal = '$15,000'
+let current = '$12,000'
+$('.goal').text(goal);
+$('#thermometer').thermometer(current);
+
